dc29ded3b2e4adcc04d37ec2e86f31fb4219b962
DeFexNN <stadnikdd@khmnu.edu.ua>
2024-11-30 18:01:21 +0300

    Fix async warning in main.py

--- main.py
+++ main.py
@@ -7,24 +7,19 @@ BOT_TOKEN = "7944589418:AAGzggoHINw9WTvnnbLC945Ax3sr8zcCWaY"
 CHAT_ID = "1424672248"  # ID вашого чату (можна дізнатися, надіславши /start боту і отримавши message.chat.id)
 
 # Ініціалізація бота
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
 
-# Функція для відправки повідомлення
-def send_message():
-    try:
-        bot.send_message(chat_id=CHAT_ID, text="Це нагадування! 🚀")
-        print("Повідомлення надіслано!")
-    except Exception as e:
-        print(f"Помилка: {e}")
+async def send_reminder():
+    await bot.send_message(chat_id=CHAT_ID, text="Це нагадування! 🚀")
+    print("Повідомлення надіслано!")
 
-# Розклад для циклічних повідомлень
-schedule.every(4).hours.do(send_message)
+def job():
+    asyncio.run(send_reminder())
 
-if __name__ == "__main__":
-    # Запускаємо нескінченний цикл
-    print("Бот запущено. Очікування...")
-    send_message()  # Надсилаємо перше повідомлення одразу
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+every(4).hours.do(job)
+
+print("Бот запущено. Очікування...")
+while True:
+    run_pending()
+    time.sleep(1)
 
